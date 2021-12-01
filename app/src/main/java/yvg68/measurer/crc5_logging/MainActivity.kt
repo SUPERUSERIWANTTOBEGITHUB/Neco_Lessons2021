378391cddacbeedce80d46ff67f898ade6004ba7
AS <yvg68@yandex.ru>
2021-12-01 19:48:43 +0300

    other

--- app/src/main/java/yvg68/measurer/crc5_logging/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/crc5_logging/MainActivity.kt
@@ -8,8 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         Log.d("MainActivityLog", "onCreate")
     }
+
 
     override fun onStart() {
         super.onStart()
