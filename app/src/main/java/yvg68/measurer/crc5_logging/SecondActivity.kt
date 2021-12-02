e48248ec5b6a94e6361f25647b3baae583ad5f74
AS <yvg68@yandex.ru>
2021-12-02 18:12:55 +0300

    "create first intent there-here"

--- app/src/main/java/yvg68/measurer/crc5_logging/SecondActivity.kt
+++ app/src/main/java/yvg68/measurer/crc5_logging/SecondActivity.kt
@@ -1,20 +1,31 @@
 package yvg68.measurer.crc5_logging
 //second activity_
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
+private const val HELLO_KEY = "hello"
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        lateinit var buttonBack: Button
         var textViewSecond = findViewById<TextView>(R.id.textViewSecond)
         textViewSecond.text = "Starting STATE"
-        var buttonBack = findViewById<Button>(R.id.buttonBack)
+        buttonBack = findViewById<Button>(R.id.buttonBack)
         buttonBack.text = "BACK"
+        // // Here starting "Intent "
+        buttonBack = findViewById(R.id.buttonBack)
+        buttonBack.setOnClickListener {
+            var MainActivityIntent = Intent(this, MainActivity::class.java)
+            MainActivityIntent.putExtra(HELLO_KEY, "This second activity application")
+            startActivity(MainActivityIntent)
+        }
     }
+        
     fun onClickButtonBack(view:View){
         var buttonBack = findViewById<Button>(R.id.buttonBack)
         buttonBack.text = "BACK"
