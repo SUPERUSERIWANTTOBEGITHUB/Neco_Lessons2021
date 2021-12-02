e749cafc41f7f78c0418d93eb70318686b555129
AS <yvg68@yandex.ru>
2021-12-02 12:07:21 +0300

    add buttons RGHIT and LEFT and textView

--- app/src/main/java/yvg68/measurer/crc5_logging/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/crc5_logging/MainActivity.kt
@@ -3,17 +3,46 @@ package yvg68.measurer.crc5_logging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MainActivityLog", "Печаль и радость, смех и слёзы,")
+        var textView = findViewById<TextView>(R.id.textViewMsg)
+        textView.text = "Starting STATE"
+        var buttonRight = findViewById<Button>(R.id.buttonRight)
+        buttonRight.text = "PUSH"
+        var buttonLeft = findViewById<Button>(R.id.buttonLeft)
+        buttonLeft.text = "OK"
+
+    }
+    fun onClickButtonRight(view: View){
+        var textView = findViewById<TextView>(R.id.textViewMsg)
+        var buttonRight = findViewById<Button>(R.id.buttonRight)
+        var buttonLeft = findViewById<Button>(R.id.buttonLeft)
+        textView.text = "PUSHED RIGHT BUTTON"
+        buttonRight.text = "OK"
+        buttonLeft.text = "PUSH"
+
+    }
+    fun onClickButtonLeft(view: View){
+        var textView = findViewById<TextView>(R.id.textViewMsg)
+        var buttonRight = findViewById<Button>(R.id.buttonRight)
+        var buttonLeft = findViewById<Button>(R.id.buttonLeft)
+        textView.text = "PUSHED LEFT BUTTON"
+        buttonRight.text = "PUSH"
+        buttonLeft.text = "OK"
+
     }
 
     override fun onStart() {
         super.onStart()
         Log.d("MainActivityLog", "Огонь сердец и песня чувства,")
+
 
     }
 
