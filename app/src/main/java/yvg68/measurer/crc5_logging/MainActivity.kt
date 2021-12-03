e1969787d93281885532ce0e679e3c35ac13f123
AS <yvg68@yandex.ru>
2021-12-03 20:21:17 +0300

    wrong commit

--- app/src/main/java/yvg68/measurer/crc5_logging/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/crc5_logging/MainActivity.kt
@@ -9,12 +9,15 @@ import android.widget.TextView
 import android.view.View
 // val for Intent
 private const val HELLO_KEY = "hello"
+
+public var TOTAL_COUNT: String = "0"
+var count:Int  = 0
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MainActivityLog", "Печаль и радость, смех и слёзы,")
-        // varia,le for Intent
+        // variable for Intent
         lateinit var buttonNext: Button
         var textView = findViewById<TextView>(R.id.textViewMsg)
         textView.text = "THE STARTING STATE FIRST ACTIVITY."
@@ -28,11 +31,30 @@ class MainActivity : AppCompatActivity() {
         // // Here starting "Intent "
         buttonNext = findViewById(R.id.buttonNext)
         buttonNext.setOnClickListener {
-            var secondActivityIntent = Intent(this, SecondActivity ::class.java)
-            secondActivityIntent.putExtra(HELLO_KEY, "This second activity application")
-            startActivity(secondActivityIntent)
+            //var secondActivityIntent = Intent(this, SecondActivity ::class.java)
+            //secondActivityIntent.putExtra(HELLO_KEY, "This second activity application")
+            //var textViewSecond = findViewById<TextView>(R.id.textViewSecond)
+            //textViewSecond.text = "THIS SCREEN OPENED"
+            var textCount:String
+            //count:Int = 5 //= Integer.parseInt(countString.text.toString())
+            count++
+            
+            textCount = count.toString()
+            val countIntent = Intent(this, SecondActivity::class.java)
+            countIntent.putExtra(SecondActivity.TOTAL_COUNT, textCount)
+            startActivity(countIntent)
+            //startActivity(secondActivityIntent)
            }
-
+    fun onClickButtonNext(view: View){
+        val textCount: String //= findViewById<TextView>(R.id.textViewRandom)
+        val countString = findViewById<TextView>(R.id.textViewRandom)
+        count = 2 //= Integer.parseInt(countString.text.toString())
+        count++
+        textCount = count.toString()
+        val countIntent = Intent(this, SecondActivity::class.java)
+        countIntent.putExtra(SecondActivity.TOTAL_COUNT, textCount)
+        startActivity(countIntent)
+    }
 
     }
     fun onClickButtonRight(view: View){
@@ -48,12 +70,16 @@ class MainActivity : AppCompatActivity() {
         var textView = findViewById<TextView>(R.id.textViewMsg)
         var buttonRight = findViewById<Button>(R.id.buttonRight)
         var buttonLeft = findViewById<Button>(R.id.buttonLeft)
+        var buttonLeftBackground = findViewById<Button>(R.id.buttonLeft)
+        //buttonLeftBackground.background =
+        var buttonRightBackground = findViewById<Button>(R.id.buttonRight)
+        //buttonRightBackground.background =
         textView.text = "PUSHED LEFT BUTTON"
         buttonRight.text = "PUSH"
         buttonLeft.text = "DONT PUSH"
 
     }
-    fun onClickButtonBack(view:View){
+    fun onClickButtonNext(view:View){
         
     }
     override fun onStart() {
--- app/src/main/java/yvg68/measurer/crc5_logging/SecondActivity.kt
+++ app/src/main/java/yvg68/measurer/crc5_logging/SecondActivity.kt
@@ -9,19 +9,28 @@ import android.widget.TextView
 
 private const val HELLO_KEY = "hello"
 class SecondActivity : AppCompatActivity() {
+    companion object{
+        var TOTAL_COUNT = "0f"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         lateinit var buttonBack: Button
         var textViewSecond = findViewById<TextView>(R.id.textViewSecond)
-        textViewSecond.text = "Starting STATE SECOND ACTIVITY"
+        textViewSecond.text = "YOU SEE THIS DISPLAY"
         buttonBack = findViewById<Button>(R.id.buttonBack)
         buttonBack.text = "BACK"
+        var textViewCount = findViewById<TextView>(R.id.textViewRandom)
+        //textViewCount.text = TOTAL_COUNT
+        var countReseive = intent.getStringExtra(TOTAL_COUNT)
+        textViewCount.text = countReseive
         // // Here starting "Intent "
         buttonBack = findViewById(R.id.buttonBack)
         buttonBack.setOnClickListener {
             var MainActivityIntent = Intent(this, MainActivity::class.java)
             MainActivityIntent.putExtra(HELLO_KEY, "There was a transition from the second activity")
+            //var textView = findViewById<TextView>(R.id.textViewMsg)
+            //textView.text = "HI from SECOND ACTIVITY"
             startActivity(MainActivityIntent)
         }
     }
